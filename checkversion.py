3ed2c5144636061c86bc2e8701c8792fca0c8417
ookmm <gaylordmbuyi@gmail.com>
2017-01-10 09:02:36 -0700

    Minor change

--- checkversion.py
+++ checkversion.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import requests
 
-print (requests.__version__)
+print(requests.__version__)
 
 response = requests.get("http://google.com")
 
 print(response.status_code)
-print (response.text)
+print(response.text)
